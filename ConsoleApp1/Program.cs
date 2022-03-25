b60ad059a5b25f281ea91e392d103c75e0873b6a
3109917 <3109917@TE-GDA-D-D12C18>
2022-03-25 12:10:15 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿string a;
+Console.WriteLine("wpisz tekst");
+a = Console.ReadLine();
+int b;
+Console.WriteLine("WPisz o ile chcesz przesunac");
+b = Int32.Parse(Console.ReadLine());
+
+char[] tab = a.ToCharArray();
+bool space;
+
+for (int i = 0; i < tab.Length; i++)
+{
+    space = Char.IsWhiteSpace(tab[i]);
+    if (space == true) {
+        continue;
+    }
+    else
+    {
+        tab[i] = (char)((char)tab[i] + b);
+    }
+
+
+}
+string wyjscie = string.Join("", tab);
+Console.WriteLine("Zaszyfrowany kod to: " + wyjscie);
